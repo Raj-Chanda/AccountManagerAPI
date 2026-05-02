310b0638722a227080837abfb4127288d6c068a1
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-03 00:10:21 +0530

    Created DbContext and DbSet

---
+++ src/AccountManagement.Domain/DB/AccountManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManagement.Domain.DB
+{
+    internal class AccountManagementDbContext : DbContext
+    {
+        public AccountManagementDbContext(DbContextOptions<AccountManagementDbContext> options)
+        {
+
+        }
+
+        public DbSet<UserDetail> UserDetails { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
