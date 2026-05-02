90a4ce617fcf037b3a6d5861e7698c68ebda5f1e
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-03 00:09:50 +0530

    Added Controller File Only

---
+++ src/AccountManagement.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
