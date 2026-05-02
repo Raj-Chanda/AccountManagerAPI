026d165b8425b0f0bff5f1db7877ce425093b472
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-03 00:11:44 +0530

    User Dto Added

---
+++ src/AccountManagement.Domain/Dto/AddUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManagement.Domain.Dto
+{
+    public class AddUserRequest
+    {
+        public sttring UserId { get; set; }
+    }
+}
---
+++ src/AccountManagement.Domain/Dto/AddUserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManagement.Domain.Dto
+{
+    public class AddUserResponse
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Credit { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
