44a0ab20ae1ef1527957aa6095595236305e6ea0
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-03 17:53:30 +0530

    Added Db Context

--- src/AccountManagement.Domain/DB/AccountManagementDbContext.cs
+++ src/AccountManagement.Domain/DB/AccountManagementDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AccountManagement.Domain.Enum;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +8,10 @@ namespace AccountManagement.Domain.DB
 {
     internal class AccountManagementDbContext : DbContext
     {
-        public AccountManagementDbContext(DbContextOptions<AccountManagementDbContext> options)
-        {
-
-        }
+        public AccountManagementDbContext(DbContextOptions<AccountManagementDbContext> options) : base(options) { }
 
         public DbSet<UserDetail> UserDetails { get; set; }
         public DbSet<Account> Accounts { get; set; }
+        public DbSet<PassBook> PassBooks { get; set; }
     }
 }
